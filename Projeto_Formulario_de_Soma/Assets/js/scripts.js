2db1bca8f78c6eaab7a18d6a1de93632579c9431
Maria Caroline <projetos.crrz@gmail.com>
2022-02-04 22:59:20 -0300

    Andamento

--- Projeto_Formulario_de_Soma/Assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-var n1 = document.querySelector('#n1')
-var n2 = document.querySelector('#n2')
-var resultado = document.querySelector('span')
-
-function somar() {
-    resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
-}
